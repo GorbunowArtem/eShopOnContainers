6127f36ee9b23b1d2546ce9a4e416e0704b002f8
Artem Horbunov <gorbunowartemwork@gmail.com>
2021-09-09 20:25:01 +0300

    Added Coupon.FunctionalTests project

---
+++ src/Services/Coupon/Coupon.FunctionalTests/CouponScenarioBase.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Coupon.API;
+using Coupon.API.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Coupon.FunctionalTests
+{
+	public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+	{
+		public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, 
+			ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+			: base(options, logger, encoder, clock)
+		{
+		}
+
+		protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+		{
+			var claims = new[] { new Claim(ClaimTypes.Name, "Test user") };
+			var identity = new ClaimsIdentity(claims, "Test");
+			var principal = new ClaimsPrincipal(identity);
+			var ticket = new AuthenticationTicket(principal, "Test");
+
+			var result = AuthenticateResult.Success(ticket);
+
+			return Task.FromResult(result);
+		}
+	}
+	public class CouponScenarioBase
+	{
+		public TestServer CreateServer()
+		{
+			var path = Assembly.GetAssembly(typeof(CouponContext)).Location;
+
+			var hostBuilder = new WebHostBuilder()
+				.UseContentRoot(Path.GetDirectoryName(path))
+				.ConfigureAppConfiguration(cb =>
+				{
+					cb.AddJsonFile("appsettings.json", optional: false)
+						.AddEnvironmentVariables();
+				})
+				.ConfigureServices(services =>
+				{
+					services.AddAuthentication("Test")
+						.AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+							"Test", options => {});
+				})
+				.UseStartup<Startup>();
+
+
+			var testServer = new TestServer(hostBuilder);
+			
+			return testServer;
+		}
+	}
+}
---
+++ src/Services/Coupon/Coupon.FunctionalTests/CouponScenarios.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Coupon.FunctionalTests
+{
+	public class CouponScenarios: CouponScenarioBase
+	{
+		[Fact]
+		public async Task ShouldReturnCouponByCode()
+		{
+			using (var server = CreateServer())
+			{
+				var response = await server.CreateClient()
+					.GetAsync("api/v1/coupon/DISC-10");
+
+				response.EnsureSuccessStatusCode();
+			}
+		}
+	}
+}
