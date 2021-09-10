fd2d588e256b9d7e7a1dd9cacda3f0e0cac48d99
Artem Horbunov <gorbunowartemwork@gmail.com>
2021-09-10 12:58:00 +0300

    Added Coupon integration tests

--- src/Services/Coupon/Coupon.FunctionalTests/Base/CouponScenarioBase.cs
+++ src/Services/Coupon/Coupon.FunctionalTests/Base/CouponScenarioBase.cs
@@ -1,8 +1,5 @@
 ﻿using System.IO;
 using System.Reflection;
-using System.Security.Claims;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
 using Coupon.API;
 using Coupon.API.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Authentication;
@@ -10,36 +7,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
-namespace Coupon.FunctionalTests
+namespace Coupon.FunctionalTests.Base
 {
-	public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
-	{
-		public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, 
-			ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
-			: base(options, logger, encoder, clock)
-		{
-		}
-
-		public static string DefaultScheme { get; set; } = "Test";
-
-		protected override Task<AuthenticateResult> HandleAuthenticateAsync()
-		{
-			var claims = new[] { new Claim(ClaimTypes.Name, "Test user") };
-			var identity = new ClaimsIdentity(claims, "Test");
-			var principal = new ClaimsPrincipal(identity);
-			var ticket = new AuthenticationTicket(principal, "Test");
-
-			var result = AuthenticateResult.Success(ticket);
-
-			return Task.FromResult(result);
-		}
-	}
 	public class CouponScenarioBase
 	{
-		public TestServer CreateServer()
+		protected TestServer CreateServer()
 		{
 			var path = Assembly.GetAssembly(typeof(CouponContext)).Location;
 
@@ -50,7 +23,7 @@ namespace Coupon.FunctionalTests
 					cb.AddJsonFile("appsettings.json", optional: false)
 						.AddEnvironmentVariables();
 				})
-				.ConfigureServices(services =>
+				.ConfigureTestServices(services =>
 				{
 					services.AddAuthentication(options =>
 						{
@@ -59,6 +32,7 @@ namespace Coupon.FunctionalTests
 						})
 						.AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
 							TestAuthHandler.DefaultScheme, options => { });
+
 				})
 				.UseStartup<Startup>();
 
--- src/Services/Coupon/Coupon.FunctionalTests/CouponScenarios.cs
+++ src/Services/Coupon/Coupon.FunctionalTests/CouponScenarios.cs
@@ -1,19 +1,33 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net;
+using System.Threading.Tasks;
+using Coupon.FunctionalTests.Base;
+using FluentAssertions;
 using Xunit;
 
 namespace Coupon.FunctionalTests
 {
-	public class CouponScenarios: CouponScenarioBase
+	public class CouponScenarios : CouponScenarioBase
 	{
 		[Fact]
 		public async Task ShouldReturnCouponByCode()
 		{
 			using var server = CreateServer();
-			
+
 			var response = await server.CreateClient()
 				.GetAsync("api/v1/coupon/DISC-10");
 
 			response.EnsureSuccessStatusCode();
+		}
+
+		[Fact]
+		public async Task ShouldReturnNotFoundIfCouponNotExists()
+		{
+			using var server = CreateServer();
+
+			var response = await server.CreateClient()
+				.GetAsync("api/v1/coupon/DISC-100");
+
+			response.StatusCode.Should().Be(HttpStatusCode.NotFound);
 		}
 	}
 }
